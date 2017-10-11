7953b65bb6908c25a90bd4efbfddcc3d30116ee5
Barry Marshall <barry@codetraptor.com>
2017-10-10 22:05:52 -0500

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import styled from 'styled-components';
+
+const Button = styled.button`
+	background-color: #007dff;
+	color: white;
+	padding: 4px 10px;
+	font-size: 14px;
+	border: none;
+	border-radius: 4px;
+`;
+
+export { Button };
---
+++ webpack.config.js
@@ -0,0 +1,26 @@
+const path = require('path');
+
+const paths = {
+	dist: path.resolve(__dirname, 'dist'),
+	src: path.resolve(__dirname, 'src')
+};
+
+module.exports = {
+	entry: path.join(paths.src, 'index.js'),
+	output: {
+		path: paths.dist,
+		filename: 'manaiakalani.js'
+	},
+	module: {
+		rules: [
+			{
+				test: /\.(js|jsx)$/,
+				exclude: /node_modules/,
+				use: ['babel-loader']
+			}
+		]
+	},
+	resolve: {
+		extensions: ['.js', '.jsx']
+	}
+};
